849add142ab2628ef4f3595e33ce8c775a8552f0
Jared Harbison <jared.harbison@gmail.com>
2018-12-25 20:58:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["element 1", "element 2", "element 3", "element 4"]
+  ["I", "am", "really", "learning"]
 end
 
 def add_element_to_end_of_array(array, element)
