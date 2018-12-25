244db916fdc9dc8efe55fa5c0811ac1e82a1fcb3
Jared Harbison <jared.harbison@gmail.com>
2018-12-25 20:19:35 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,7 +10,7 @@ def add_element_to_end_of_array(array, element)
   create_an_array << "arrays!"
 end
 
-def add_element_to_start_of_array
+def add_element_to_start_of_array(array, element)
   create_an_array.unshift("wow")
 end
 
