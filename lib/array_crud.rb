383e600006f899e4bd742e177599ab95632abe80
Jared Harbison <jared.harbison@gmail.com>
2018-12-25 20:40:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -16,6 +16,7 @@ end
 
 def remove_element_from_end_of_array(array)
   add_element_to_end_of_array.pop
+  puts remove_element_from_end_of_array.inspect
 end
 
 def remove_element_from_start_of_array(array)
